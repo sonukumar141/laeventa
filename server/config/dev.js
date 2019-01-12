2f80feb01f5fb60ac5fd67998e45fd078f75d5a7
Sonu Mishra <sonukumar141@gmail.com>
2019-01-13 01:16:03 +0530

    fixed untracked files

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    DB_URI: 'mongodb://skm:test123@ds215633.mlab.com:15633/laeventa-prod',
-    SECRET: 'ajdjfhaJDAKDFkjjnfwrn'
-}
--- server/config/prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-
-	DB_URI: process.env.DB_URI,
-	SECRET: process.env.SECRET
-}
