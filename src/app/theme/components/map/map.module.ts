998a73211a53dc724fecbc862af2a3c2fdd47d9d
Sonu Mishra <sonukumar141@gmail.com>
2019-01-14 15:11:45 +0530

    finishing map component

--- src/app/theme/components/map/map.module.ts
+++ src/app/theme/components/map/map.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MapComponent } from './map.component';
 import { AgmCoreModule } from '@agm/core';
+import { CamelizePipe } from 'ngx-pipes';
+import { CommonModule } from '@angular/common';
+
+import { MapService } from './map.service';
 
 @NgModule({
     declarations:[
@@ -12,10 +16,12 @@ import { AgmCoreModule } from '@agm/core';
     imports: [
         AgmCoreModule.forRoot({
             apiKey: 'AIzaSyDKhMuYssF4ls3F2idN1-NhKOf9Ov0pPl4'
-          })
+          }),
+          CommonModule
     ],
     providers: [
-
+        MapService,
+        CamelizePipe
     ]
 })
 export class MapModule {}
