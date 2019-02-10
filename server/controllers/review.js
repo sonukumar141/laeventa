d3865415d3d537c7cca1ee8542ffd3a6a5b2a0eb
Sonu Mishra <sonukumar141@gmail.com>
2019-02-11 00:12:48 +0530

    review feature started and banner images changed

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -3,7 +3,6 @@ const Job = require('../models/job');
 
 const Review = require('../models/review');
 
-const Booking = require('../models/booking');
 const moment = require('moment');
 const { normalizeErrors } = require('../helpers/mongoose');
 
@@ -18,60 +17,79 @@ exports.getReviews = function(req, res){
                 }
 
                 return res.json(reviews);
-          });
-}
+        });
+    }
+
+// exports.createReview = function(req, res) {
+//     const reviewData = req.body;
+//     const { bookingId } = req.query;
+//     const user = res.locals.user;
+
+//     Booking.findById(bookingId)
+//            .populate({path: 'job', populate: {path: 'user'}})
+//            .populate('review')
+//            .populate('user')
+//            .exec(async(err, foundBooking) => {
+//                 if(err){
+//                     return res.status(422).send({errors: normalizeErrors(err.errors)});
+//                 }          
+                
+//                 const {job} = foundBooking;
+
+//                 if(job.user.id === user.id) {
+//                     return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'Cannot create review on your Job!'}]});
+//                 }
+                
+//                 const foundBookingUserId = foundBooking.user.id;
+
+//                 if(foundBookingUserId !== user.id) {
+//                     return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'Cannot create review on other\'s booking'}]});
+//                 }
+
+
+//                 const timeNow = moment();
+//                 const endAt = moment(foundBooking.endAt);
+
+//                 if(!endAt.isBefore(timeNow)){
+//                     return res.status(422).send({errors: [{title: 'Invalid Date!', detail: 'You can review after booking is over'}]});
+//                 }
+
+//                 if(foundBooking.review) {
+//                     return res.status(422).send({errors: [{title: 'Review already done!', detail: 'You have already reviewed this booking'}]});
+//                 }               
+
+//                 const review = new Review(reviewData);
+//                 review.user = user;
+//                 review.job = job;
+//                 foundBooking.review = review;
+
+//                 try{
+//                     await foundBooking.save();
+//                     const savedReview = await review.save();
+                    
+//                     return res.json(savedReview);
+//                 }catch(err){
+//                     return res.status(422).send({errors: normalizeErrors(err.errors)});
+//                 }
+
+//            });
+// }
 
 exports.createReview = function(req, res) {
-    const reviewData = req.body;
-    const { bookingId } = req.query;
+    
+    const { rating, text } = req.body;
     const user = res.locals.user;
 
-    Booking.findById(bookingId)
-           .populate({path: 'job', populate: {path: 'user'}})
-           .populate('review')
-           .populate('user')
-           .exec(async(err, foundBooking) => {
-                if(err){
-                    return res.status(422).send({errors: normalizeErrors(err.errors)});
-                }          
-                
-                const {job} = foundBooking;
+    const review = new Review({rating, text});
+    review.user = user;
 
-                if(job.user.id === user.id) {
-                    return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'Cannot create review on your Job!'}]});
-                }
-                
-                const foundBookingUserId = foundBooking.user.id;
+    Review.create(review, function(err, newReview) {
+		if(err) {
+			return res.status(422).send({errors: normalizeErrors(err.errors)});
+		}
 
-                if(foundBookingUserId !== user.id) {
-                    return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'Cannot create review on other\'s booking'}]});
-                }
-
-
-                const timeNow = moment();
-                const endAt = moment(foundBooking.endAt);
-
-                if(!endAt.isBefore(timeNow)){
-                    return res.status(422).send({errors: [{title: 'Invalid Date!', detail: 'You can review after booking is over'}]});
-                }
-
-                if(foundBooking.review) {
-                    return res.status(422).send({errors: [{title: 'Review already done!', detail: 'You have already reviewed this booking'}]});
-                }               
-
-                const review = new Review(reviewData);
-                review.user = user;
-                review.job = job;
-                foundBooking.review = review;
-
-                try{
-                    await foundBooking.save();
-                    const savedReview = await review.save();
-                    
-                    return res.json(savedReview);
-                }catch(err){
-                    return res.status(422).send({errors: normalizeErrors(err.errors)});
-                }
-
-           });
+		User.update({_id: user.id}, {$push: {review: newReview}}, function(){});
+        
+		return res.json(newReview);
+	});
 }
